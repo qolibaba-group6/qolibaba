2ad8d1733a4064079f69a582bcee467ba1703909
Ehsan Sobhani <52486321+alefsobhani@users.noreply.github.com>
2024-12-30 17:02:35 +0330

    Update payment.go

--- internal/payment-service/model/payment.go
+++ internal/payment-service/model/payment.go
@@ -1,4 +1,3 @@
-// internal/payment-service/model/payment.go
 package model
 
 import (
@@ -12,7 +11,7 @@ type Payment struct {
 	ID            uuid.UUID `gorm:"type:uuid;primaryKey;" json:"id"`
 	UserID        uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
 	Amount        float64   `gorm:"not null" json:"amount"`
-	Status        string    `gorm:"not null" json:"status"` // مثال: pending, completed, failed, refunded
+	Status        string    `gorm:"not null" json:"status"` 
 	TransactionID string    `gorm:"unique;not null" json:"transaction_id"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
